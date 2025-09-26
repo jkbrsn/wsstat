04022487cbd96954ee3f53bd6f61842f99dde1fb
Jakob Ersson <11717405+jakobilobi@users.noreply.github.com>
2025-09-26 08:59:53 +0200

    style: move some things main -> main_helpers

--- cmd/wsstat/main.go
+++ cmd/wsstat/main.go
@@ -4,49 +4,14 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
-	"net/url"
 	"os"
 	"os/signal"
-	"strconv"
-	"strings"
 	"syscall"
 
 	"github.com/jkbrsn/wsstat/internal/app"
 )
-
-type trackedIntFlag struct {
-	value int
-	set   bool
-}
-
-func newTrackedIntFlag(defaultValue int) trackedIntFlag {
-	return trackedIntFlag{value: defaultValue}
-}
-
-func (f *trackedIntFlag) Set(s string) error {
-	v, err := strconv.Atoi(s)
-	if err != nil {
-		return err
-	}
-	f.value = v
-	f.set = true
-	return nil
-}
-
-func (f *trackedIntFlag) String() string {
-	return strconv.Itoa(f.value)
-}
-
-func (f *trackedIntFlag) Value() int {
-	return f.value
-}
-
-func (f *trackedIntFlag) WasSet() bool {
-	return f.set
-}
 
 type deprecatedAliasInt struct {
 	target *trackedIntFlag
@@ -213,60 +178,4 @@ func main() {
 
 	// Print the response, if there is one
 	ws.PrintResponse()
-}
-
-// parseValidateInput parses and validates the flags and input passed to the program.
-func parseValidateInput() (*url.URL, error) {
-	flag.Parse()
-
-	if *showVersion {
-		fmt.Printf("Version: %s\n", version)
-		os.Exit(0) //revive:disable:deep-exit allow here
-	}
-
-	if *basic && *verbose || *basic && *quiet || *verbose && *quiet {
-		return nil, errors.New("mutually exclusive verbosity flags")
-	}
-
-	if *textMessage != "" && *jsonMethod != "" {
-		return nil, errors.New("mutually exclusive messaging flags")
-	}
-
-	args := flag.Args()
-	if len(args) != 1 {
-		return nil, errors.New("invalid number of arguments")
-	}
-
-	u, err := parseWSURI(args[0])
-	if err != nil {
-		return nil, fmt.Errorf("error parsing input URI: %v", err)
-	}
-
-	return u, nil
-}
-
-// parseWSURI parses the rawURI string into a URL object.
-func parseWSURI(rawURI string) (*url.URL, error) {
-	uri := rawURI
-	if !strings.Contains(rawURI, "://") {
-		scheme := "wss://"
-		if *insecure {
-			scheme = "ws://"
-		}
-		uri = scheme + rawURI
-	}
-
-	u, err := url.Parse(uri)
-	if err != nil {
-		return nil, err
-	}
-
-	return u, nil
-}
-
-func resolveCountValue(subscribe, subscribeOnce bool) int {
-	if !countFlag.WasSet() && subscribe && !subscribeOnce {
-		return 0
-	}
-	return countFlag.Value()
 }
